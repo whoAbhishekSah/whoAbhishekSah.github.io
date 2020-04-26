29d7a4c0abbe50ff9807a96991cecacc9758a949
Abhishek Sah <abhishek.sah@go-jek.com>
2020-04-26 15:39:56 +0530

    [Abhishek] Fix year of blogs not appearing

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,11 +49,15 @@ const BlogIndex = ({ data, location }) => {
       <Introduction />
       <h3 align="center">Blog posts</h3>
       {years.map(year => {
-        <h6>{year}</h6>
         const postsOfThisYear = postsIndexedByYears[year]
-        return (postsOfThisYear.map(({ node }) => {
-          return renderPost(node)
-        }))
+        return (
+          <React.Fragment>
+            <div>{year}</div>
+            {postsOfThisYear.map(({ node }) => {
+              return renderPost(node)
+            })}
+          </React.Fragment>
+        )
       })}
     </Layout>
   )
