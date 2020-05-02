87cd00c8fa005ddd7aa679943a8bb29907352eab
Abhishek Sah <abhishek.sah@go-jek.com>
2020-05-02 17:07:52 +0530

    [Abhishek] Add shortname hardcoded

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -12,7 +12,7 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
   const siteTitle = data.site.siteMetadata.title;
   const { previous, next } = pageContext;
   const disqusConfig = {
-    shortname: process.env.GATSBY_DISQUS_NAME,
+    shortname: 'abhisheksah',
     config: { identifier: siteTitle }
   };
   return (
